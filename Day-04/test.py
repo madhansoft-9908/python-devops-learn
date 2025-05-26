4c9c48a5c156733edf16ce29aa62554b8dd9481a
madhansoft-9908 <83653110+madhansoft-9908@users.noreply.github.com>
2025-05-26 19:11:39 +0530

    Update test.py

--- Day-04/test.py
+++ Day-04/test.py
@@ -20,13 +20,37 @@ x = addition(6,4)
 print(x)
 
 #module
-#Suppose you have a Python file named my_module.py
+#Suppose you have a Python file named my_module.py --- already my_module.py file ispresent in the day-04 folder 
 # my_module.py
 def square(x):
     return x ** 2
+print(square(5))
 pi = 3.14159265
 
-#You can use the above module in another script:
+#You can use the module in another script:
+#we can import the module using import keyword
+#This is importing the module
 import my_module
-result = my_module.square(10)
-print(result)
+result = my_module.square(10)  #10 power 2 = 10*10 = 100
+print(result) #or print(my_module.square(5))
+print(my_module.square(10))
+
+#import package
+#Suppose you have a package structure as follows:
+#my_package/
+#    __init__.py
+#    module1.py
+#    module2.py
+
+#You can use modules from this package as follows:
+#This is importing the package and module from that package
+
+#from my_package import module1
+#result = module1.function_from_module1()   #function_from_module is just a function name from module1.
+
+#python workspace
+
+# 1.pip install virtualenv
+# 2.python -m venv project-abc   ----create the project
+# 3.source project-abc/bin/activate ----switch to the project
+# 4.deactivate --- come out of the project.
