11bae0a9243e6ed37fc8805030b448df4423e52e
madhansoft-9908 <83653110+madhansoft-9908@users.noreply.github.com>
2025-05-26 15:40:30 +0530

    Update test.py

--- Day-03/test.py
+++ Day-03/test.py
@@ -1,1 +1,23 @@
 #keywords and variables
+#to define a local and global variable , will show you using function
+
+#Global variable - which is defined outside of the function and can be used to any of function.
+a = 5
+b = 4
+def addition():
+    result = a + b
+    print(result)
+addition()
+
+def sub():
+    result = a - b
+    print(result)
+sub()
+print(a)
+
+#local variable ---which is specific to that function
+def addition():
+    a = 4
+    b = 2
+    print(a+b)
+addition()
