86dd405f6b5d2a234fcd2c64c7b84ef5b89be98e
madhansoft-9908 <83653110+madhansoft-9908@users.noreply.github.com>
2025-05-26 15:56:52 +0530

    Update test.py

--- Day-03/test.py
+++ Day-03/test.py
@@ -21,3 +21,31 @@ def addition():
     b = 2
     print(a+b)
 addition()
+
+#examples
+# Define configuration variables for a web server
+server_name = "my_server"
+port = 80
+is_https_enabled = True
+max_connections = 1000
+
+# Print the configuration
+print(f"Server Name: {server_name}")
+print(f"Port: {port}")
+print(f"HTTPS Enabled: {is_https_enabled}")
+print(f"Max Connections: {max_connections}")
+
+#updated configuration
+port = 443
+is_https_enabled = False
+
+print(f"updated Port: {port}")
+print(f"updated Https enabled: {is_https_enabled}")
+
+
+#1. f"...{...}" — f-strings (formatted string literals):
+#Introduced in Python 3.6, f-strings allow you to embed variables or expressions directly inside string literals.
+#Advantage: It's cleaner and more readable, especially when formatting longer strings or inserting multiple variables.
+user = "Alice"
+age = 30
+print(f"User: {user}, Age: {age}")
