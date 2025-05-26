aca683982b8fcbdee3fb04aa88871946ccc73a43
madhansoft-9908 <83653110+madhansoft-9908@users.noreply.github.com>
2025-05-26 18:39:53 +0530

    Update test.py

--- Day-04/test.py
+++ Day-04/test.py
@@ -1,1 +1,26 @@
+#functions, packages and modules
+#this is the normal approach in function
+a = 5
+b = 4
+def addition():
+    add = a + b
+    print(add)
+addition()
 
+#below is the best approach
+#takes the input
+#perform the required action
+#returns the output
+def addition(a,b):
+    add = a + b
+    return add
+print(addition(10,4))
+#    or
+x = addition(6,4)
+print(x)
+
+#example
+# my_module.py
+def square(x):
+    return x ** 2
+pi = 3.14159265
