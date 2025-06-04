2a9741584bb54401bba514755dc2a99eb8e930fc
madhansoft-9908 <83653110+madhansoft-9908@users.noreply.github.com>
2025-06-04 19:01:28 +0530

    Create test.py

---
+++ Day-06/test.py
@@ -0,0 +1,48 @@
+#operators
+#Arithmatic operator
+#1.Addition (+): Adds two numbers.
+#2.Subtraction (-): Subtracts the right operand from the left operand.
+#3.Multiplication (*): Multiplies two numbers.
+#4.Division (/): Divides the left operand by the right operand (results in a floating-point number).
+#5.Floor Division (//): Divides the left operand by the right operand and rounds down to the nearest whole number.
+#6.Modulus (%): Returns the remainder of the division of the left operand by the right operand.
+#7.Exponentiation ():** Raises the left operand to the power of the right operand.
+
+a = 6
+b = 3
+addition = a+b
+print(addition)  #output = 9
+
+sub = a-b
+print(sub) #output = 3
+
+mul = a*b
+print(mul) #output = 18
+
+div = a/b 
+print(div) #output = 2.0
+
+floordiv = a//b
+print(floordiv)  #output = 2
+
+modulus = a%b
+print(modulus) #output = 0
+
+exponent = a**b
+print(exponent) #output(6*6*6) = 216
+
+#Assignment Operator
+#1.Basic Assignment (=): Assigns a value to a variable.
+#2.Addition Assignment (+=): Adds the right operand to the left operand and assigns the result to the left operand.
+#3.Subtraction Assignment (-=): Subtracts the right operand from the left operand and assigns the result to the left operand.
+#4.Multiplication Assignment (*=): Multiplies the left operand by the right operand and assigns the result to the left operand.
+#5.Division Assignment (/=): Divides the left operand by the right operand and assigns the result to the left operand.
+#6.Floor Division Assignment (//=): Performs floor division on the left operand and assigns the result to the left operand.
+#7.Modulus Assignment (%=): Calculates the modulus of the left operand and assigns the result to the left operand.
+#8.Exponentiation Assignment (=):** Raises the left operand to the power of the right operand and assigns the result to the left operand.
+
+#Basic assignment x =5
+#addition assignment
+y = 10
+y += 2 #equivalent to y = y+2
+print(y) #output = 12
