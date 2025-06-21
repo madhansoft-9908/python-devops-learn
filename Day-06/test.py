2280e6a8dc1bbf6d82fb9d8b8ccc445826f0952c
madhansoft-9908 <83653110+madhansoft-9908@users.noreply.github.com>
2025-06-21 06:51:41 +0530

    Update test.py

--- Day-06/test.py
+++ Day-06/test.py
@@ -46,3 +46,95 @@ print(exponent) #output(6*6*6) = 216
 y = 10
 y += 2 #equivalent to y = y+2
 print(y) #output = 12
+
+#relational operators
+
+#Relational operators in Python are used to compare two values and determine the relationship between them. These operators return a Boolean result, which is either True or False.
+#Equal to (==): Checks if two values are equal.
+#Not equal to (!=): Checks if two values are not equal.
+#Greater than (>): Checks if the left operand is greater than the right operand.
+#Less than (<): Checks if the left operand is less than the right operand.
+#Greater than or equal to (>=): Checks if the left operand is greater than or equal to the right operand.
+#Less than or equal to (<=): Checks if the left operand is less than or equal to the right operand.
+
+a = 5
+b = 5
+c = 10
+result = a == b
+print(result)
+
+res = a != c 
+print(res)
+res1 = a == c 
+print(res1)
+print(c > a)
+print(b < c)
+print(a >= b)
+print(a <= c)
+
+#Examples
+#Equal to
+a = 5
+b = 5
+result = a == b
+# result will be True
+#Not equal to
+x = 10
+y = 7
+result = x != y
+# result will be True
+
+#Logical Operations in Python
+#Logical operators in Python are used to manipulate and combine Boolean values. These operators allow you to perform logical operations such as AND, OR, and NOT.
+#AND (and): Returns True if both operands are True.
+#OR (or): Returns True if at least one of the operands is True.
+#NOT (not): Returns the opposite Boolean value of the operand.
+#Examples
+#AND Operator
+x = True
+y = False
+result = x and y
+# result will be False
+#OR Operator
+a = True
+b = False
+result = a or b
+# result will be True
+
+a = True
+
+result = not a
+print("not a =", result) # returns the opposite value
+
+#Identity Operations
+#dentity operators in Python are used to compare the memory locations of two objects to determine if they are the same object or not. The two identity operators are "is" and "is not."
+#is: Returns True if both operands refer to the same object.
+#is not: Returns True if both operands refer to different objects.
+#Examples
+#is Operator
+x = [1, 2, 3]
+y = x  # y now refers to the same object as x
+result = x is y
+# result will be True
+#is not Operator
+a = "hello"
+b = "world"
+result = a is not b
+# result will be True
+
+a = 4
+b = 5
+result = a is not b
+print("is not operator:",result) #returns true
+
+words = ["apple","carrot","banana"]
+output = "apple" in words
+print("membership in operator:",output)
+
+output2 = "grapes" not in words
+print("membership not in operator:",output2)
+
+output2 = "grapes" in words
+print("membership not in operator:",output2)
+
+#Precedence of Operations will check later
